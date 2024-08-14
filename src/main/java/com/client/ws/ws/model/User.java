851f471eb3717249faf5e4f8c273f17c2c11085c
vagne <vagnerdw1@gmail.com>
2024-08-14 16:09:11 -0300

    Adiciona modelagens ao projeto

---
+++ src/main/java/com/client/ws/ws/model/User.java
@@ -0,0 +1,44 @@
+package com.client.ws.ws.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "users_id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String cpf;
+
+    @Column(name = "dt_subscription")
+    private LocalDate dtSubscription = LocalDate.now();
+
+    @Column(name = "dt_expiration")
+    private LocalDate dtExpiration;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_type_id")
+    private UserType userType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subscriptions_type_id")
+    private SubscriptionType subscriptionType;
+
+}
