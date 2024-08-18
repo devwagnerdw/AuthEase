bb72ad4aeb88b69a6adf437bd1f3b2f2babbeed9
vagne <vagnerdw1@gmail.com>
2024-08-18 17:59:15 -0300

    Cria recurso para deletar SubscriptionType

--- src/main/java/com/client/ws/ws/Service/impl/SubscriptionTypeService/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/client/ws/ws/Service/impl/SubscriptionTypeService/SubscriptionTypeServiceImpl.java
@@ -60,7 +60,8 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     @Override
     public void delete(Long id) {
-
+        getSubscriptionType(id);
+        subscriptionTypeRepository.deleteById(id);
     }
 
 
