42ffa9737d3946bc12d2b0fb46b93366866584c7
vagne <vagnerdw1@gmail.com>
2024-08-18 22:43:16 -0300

    Adiciona validações ao SubscriptionTypeDto

--- src/main/java/com/client/ws/ws/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/ws/dto/SubscriptionTypeDto.java
@@ -1,27 +1,38 @@
 package com.client.ws.ws.dto;
 
 
-import com.client.ws.ws.model.SubscriptionType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
+import lombok.Builder;
 
+
+import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SubscriptionTypeDto {
 
     private Long id;
 
+    @NotBlank(message = "campo name não pode ser nulo ou vazio")
+    @Size(min = 5, max = 30)
     private String name;
 
-    private Long accessMonth;
+    @Max(value = 12, message = "campo accessMonths não pode ser maior que 12")
+    private Long accessMonths;
 
+    @NotNull(message = "campo price não pode ser nulo")
     private BigDecimal price;
 
+    @NotBlank(message = "campo productKey não pode ser nulo ou vazio")
+    @Size(min = 5, max = 15, message = "campo productKey deve ter tamanho entre 5 e 15")
     private String productKey;
-
 }
