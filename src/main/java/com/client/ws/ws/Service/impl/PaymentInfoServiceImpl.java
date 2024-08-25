d7fea0f525e1843e7637340f40dcf3ab664418a2
vagne <vagnerdw1@gmail.com>
2024-08-25 18:57:58 -0300

    adiciona status code e verifica usuario

--- src/main/java/com/client/ws/ws/Service/impl/PaymentInfoServiceImpl.java
+++ src/main/java/com/client/ws/ws/Service/impl/PaymentInfoServiceImpl.java
@@ -4,21 +4,35 @@ package com.client.ws.ws.Service.impl;
 import com.client.ws.ws.Service.PaymentInfoService;
 import com.client.ws.ws.dto.PaymentProcessDto;
 
+import com.client.ws.ws.exception.BusinessException;
+import com.client.ws.ws.exception.NotFoudException;
+import com.client.ws.ws.model.User;
 import com.client.ws.ws.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 
 @Service
 public class PaymentInfoServiceImpl implements PaymentInfoService {
 
+    private final UserRepository userRepository;
 
-    PaymentInfoServiceImpl(){
-
+    PaymentInfoServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
     }
 
     @Override
     public Boolean process(PaymentProcessDto dto) {
         //verifica usuario por id
+        var userOpt = userRepository.findById(dto.getUserPaymentInfoDto().getUserId());
+        if(userOpt.isEmpty()) {
+            throw new NotFoudException("Usuário não encontrado");
+        }
+        User user = userOpt.get();
+        if(Objects.nonNull(user.getSubscriptionType())) {
+            throw new BusinessException("Pagamento não pode ser processado pois usuário já possui assinatura");
+        }
         //salvar informacoes de pagamento
         //cria ou atualiza usuario raspay
         //cria o pedido de pagamento
---
+++ src/main/java/com/client/ws/ws/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.client.ws.ws.exception;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
