56104df8a5a60954fe5fa67fadc9c37b372f0e43
vagne <vagnerdw1@gmail.com>
2024-09-06 12:46:57 -0300

    Aplica o @cacheble e @EnableCaching

--- src/main/java/com/client/ws/ws/WsApplication.java
+++ src/main/java/com/client/ws/ws/WsApplication.java
@@ -2,8 +2,10 @@ package com.client.ws.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class WsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/client/ws/ws/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/ws/controller/SubscriptionTypeController.java
@@ -5,6 +5,7 @@ import com.client.ws.ws.dto.SubscriptionTypeDto;
 import com.client.ws.ws.model.SubscriptionType;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,13 @@ public class SubscriptionTypeController {
     @Autowired
     private SubscriptionTypeService subscriptionTypeService;
 
+    @Cacheable(value = "subscriptionType")
     @GetMapping()
     public ResponseEntity<List<SubscriptionType>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findAll());
     }
 
+    @Cacheable(value = "subscriptionType", key = "#id")
     @GetMapping("/{id}")
     public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findById(id));
