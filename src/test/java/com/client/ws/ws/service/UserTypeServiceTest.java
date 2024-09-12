9404c164f562e340447e4a2d7e6088f5ab6282bc
vagne <vagnerdw1@gmail.com>
2024-09-12 16:31:36 -0300

    Aplica extendWith e Mock

--- src/test/java/com/client/ws/ws/service/UserTypeServiceTest.java
+++ src/test/java/com/client/ws/ws/service/UserTypeServiceTest.java
@@ -1,33 +1,38 @@
 package com.client.ws.ws.service;
 
-import com.client.ws.ws.Service.UserTypeService;
+import com.client.ws.ws.Service.impl.UserTypeServiceImpl;
 import com.client.ws.ws.model.jpa.UserType;
+import com.client.ws.ws.repository.jpa.UserTypeRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserTypeServiceTest {
 
-    @MockBean
-    private UserTypeService userTypeService;
+    @Mock
+    private UserTypeRepository userTypeRepository;
+
+    @InjectMocks
+    private UserTypeServiceImpl userTypeService;
 
     @Test
     void findAll() {
         List<UserType> userTypeList = new ArrayList<>();
-
         UserType userType1 = new UserType(1l, "Professor","Professor da plataforma");
         UserType userType2 = new UserType(2l, "Administrador", "Funcionario");
         userTypeList.add(userType1);
         userTypeList.add(userType2);
 
-        Mockito.when(userTypeService.findAll()).thenReturn(userTypeList);
+        Mockito.when(userTypeRepository.findAll()).thenReturn(userTypeList);
         var result = userTypeService.findAll();
-        Assertions.assertThat(result).isNotEmpty();
+        Assertions.assertThat(result).hasSize(2).isNotEmpty();
     }
 }
