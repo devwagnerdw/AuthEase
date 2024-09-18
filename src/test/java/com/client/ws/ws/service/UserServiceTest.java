0c93a7ba537e7c7679b5279fb1521bb52406b3f9
vagne <vagnerdw1@gmail.com>
2024-09-18 15:10:14 -0300

    Cenarios de exception

--- src/test/java/com/client/ws/ws/service/UserServiceTest.java
+++ src/test/java/com/client/ws/ws/service/UserServiceTest.java
@@ -2,11 +2,14 @@ package com.client.ws.ws.service;
 
 import com.client.ws.ws.Service.impl.UserServiceImpl;
 import com.client.ws.ws.dto.UserDto;
+import com.client.ws.ws.exception.BadRequestException;
+import com.client.ws.ws.exception.NotFoudException;
 import com.client.ws.ws.model.jpa.User;
 import com.client.ws.ws.model.jpa.UserType;
 import com.client.ws.ws.repository.jpa.UserRepository;
 import com.client.ws.ws.repository.jpa.UserTypeRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,27 +22,31 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-
     @Mock
-    private UserRepository userRepository;
-
+    private  UserRepository userRepository;
     @Mock
-    private UserTypeRepository userTypeRepository;
-
+    private  UserTypeRepository userTypeRepository;
     @InjectMocks
     private UserServiceImpl userService;
 
-    @Test
-    void given_create_when_idIsNotNullAndUserIsFound_then_returnUserCreated(){
+    private static  UserDto dto;
 
-        UserDto dto = new UserDto();
-        dto.setEmail("vagne@email.com");
-        dto.setCpf("11111121311");
+    @BeforeAll
+    public static   void loadUser() {
+        dto = new UserDto();
+        dto.setId(1L);
+        dto.setEmail("felipe@email.com");
+        dto.setCpf("11111111111");
         dto.setUserTypeId(1L);
+    }
 
+    @Test
+    void given_create_when_idIsNullAndUserTypeIsFound_then_returnUserCreated(){
         UserType userType = new UserType(1L, "Aluno", "Aluno da plataforma");
 
         when(userTypeRepository.findById(1L)).thenReturn(Optional.of(userType));
+
+        dto.setId(null);
 
         User user = new User();
         user.setEmail(dto.getEmail());
@@ -48,12 +55,30 @@ class UserServiceTest {
         user.setDtExpiration(dto.getDtExpiration());
         user.setUserType(userType);
         when(userRepository.save(user)).thenReturn(user);
-
         Assertions.assertEquals(user,userService.create(dto));
-
         verify(userTypeRepository, times(1)).findById(1L);
         verify(userRepository, times(1)).save(user);
     }
 
+    @Test
+    void given_create_when_idIsNotNull_then_throwBadRequestException(){
 
+
+
+        Assertions.assertThrows(BadRequestException.class, () -> userService.create(dto));
+
+        verify(userTypeRepository, times(0)).findById(any());
+        verify(userRepository, times(0)).save(any());
+    }
+
+    @Test
+    void given_create_when_idIsNullAndUserTypeIsNotFound_then_throwNotFoudException(){
+        dto.setId(null);
+        when(userTypeRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NotFoudException.class, () -> userService.create(dto));
+
+        verify(userTypeRepository, times(1)).findById(1L);
+        verify(userRepository, times(0)).save(any());
+    }
 }
