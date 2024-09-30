a69d1fce9c62bb69dd45c84069e59cfbd8ff709e
vagne <vagnerdw1@gmail.com>
2024-09-30 09:34:50 -0300

    Melhora teste controller

--- src/test/java/com/client/ws/ws/controller/UserTypeControllerTest.java
+++ src/test/java/com/client/ws/ws/controller/UserTypeControllerTest.java
@@ -6,9 +6,13 @@ import com.client.ws.ws.model.jpa.UserType;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -17,8 +21,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+@WebMvcTest(UserTypeController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles(profiles = "test")
 class UserTypeControllerTest {
 
     @Autowired
@@ -38,8 +44,8 @@ class UserTypeControllerTest {
         System.out.println(userTypeList);
         Mockito.when(userTypeService.findAll()).thenReturn(userTypeList);
 
-//        mockMvc.perform(MockMvcRequestBuilders.get("/user-type"))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
+       mockMvc.perform(MockMvcRequestBuilders.get("/user-type"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
