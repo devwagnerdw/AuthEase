9287c9ffbe74536ef591f54c545dec062f8e8cc3
vagne <vagnerdw1@gmail.com>
2024-10-01 16:30:41 -0300

    teste findAll subscriptionType

---
+++ src/test/java/com/client/ws/ws/controller/SubscriptionTypeControllerTest.java
@@ -0,0 +1,37 @@
+package com.client.ws.ws.controller;
+
+
+import com.client.ws.ws.Service.SubscriptionTypeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@WebMvcTest(SubscriptionTypeController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles(profiles = "test")
+class SubscriptionTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SubscriptionTypeService subscriptionTypeService;
+
+    @Test
+    void given_findAll_then_returnAllUserType() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/subscription-type"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
