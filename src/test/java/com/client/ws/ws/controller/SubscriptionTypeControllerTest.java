fb182fa2976ac0bbea2211e22003d516186acdd1
vagne <vagnerdw1@gmail.com>
2024-10-02 08:21:43 -0300

    findById

--- src/test/java/com/client/ws/ws/controller/SubscriptionTypeControllerTest.java
+++ src/test/java/com/client/ws/ws/controller/SubscriptionTypeControllerTest.java
@@ -2,21 +2,27 @@ package com.client.ws.ws.controller;
 
 
 import com.client.ws.ws.Service.SubscriptionTypeService;
+import com.client.ws.ws.model.jpa.SubscriptionType;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-
-@AutoConfigureDataJpa
 @AutoConfigureTestDatabase
 @WebMvcTest(SubscriptionTypeController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -30,8 +36,20 @@ class SubscriptionTypeControllerTest {
     private SubscriptionTypeService subscriptionTypeService;
 
     @Test
-    void given_findAll_then_returnAllUserType() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/subscription-type"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    void given_findAll_then_returnAllSubscriptionTYpe() throws Exception {
+        mockMvc.perform(get("/subscription-type"))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void given_findById_whenGetId2_then_returnOneSubscriptionType() throws Exception {
+        SubscriptionType subscriptionType = new SubscriptionType(2L, "VITALICIO", null,
+                BigDecimal.valueOf(997), "FOREVER2022");
+        Mockito.when(subscriptionTypeService.findById(2L)).thenReturn(subscriptionType);
+        mockMvc.perform(get("/subscription-type/2").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(2)))
+                .andExpect(jsonPath("$.name", is("VITALICIO")))
+        ;
     }
 }
